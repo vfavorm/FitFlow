5fe80bb6258896f9f87f8b0dd73bf0293132e6f8
Favor Vincent <vfavorm77@gmail.com>
2026-01-07 18:00:56 +0300

    Update frontend API URL in CORS

--- Backend/app.py
+++ Backend/app.py
@@ -34,7 +34,7 @@ api = Api(app)
 jwt = JWTManager(app)
 
 CORS(app, 
-     origins="https://fit-flow-gym-management-git-main-favors-projects-47fb2718.vercel.app",
+     origins="https://fit-flow-omega.vercel.app",
      supports_credentials=True,
      allow_headers=["Content-Type", "Authorization"],
      methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"])
